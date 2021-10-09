9c12965d4df6d2d332eb073cbbf59c9be8889473
Andrei9383 <andreistan275@gmail.com>
2021-10-09 19:09:38 +0300

    Maxime si minime

---
+++ Algoritmi elementari/Maxime si minime/minime.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define ull unsigned long long
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+    int n, min = INF, min2 = INF, min3 = INF, x; cin >> n;
+    for(int i = 1; i<=n; i++){
+        cin >> x;
+        if(x < min){
+            min3 = min2;
+            min2 = min;
+            min = x;
+        }
+        else if(x < min2){
+            min3 = min2;
+            min2 = x;
+        }
+        else if(x < min3){
+            min3 = x;
+        }
+    }
+    cout << min3 << " " << min2 << " " << min;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    //cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        //cout << "Case #" << t << ": ";
+        solve();
+    }
+}
